611548245a50206019eeb00c1ebe1cc3b21e69d2
Anastasiya Fedotova <nastenaf@list.ru>
2022-08-22 22:26:55 +0300

    13 Пр

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
+/* eslint-disable no-console */
 // app.js — входной файл
-
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mydb', {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-  useCreateIndex: true,
-    useFindAndModify: false
 });
 
-// подключаем мидлвары, роуты и всё остальное...
+app.use((req, res, next) => {
+  req.user = {
+    _id: '630332513ed5079d05f09aed',
+  };
 
-app.listen(3000);
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.use('/', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ controllers/cards.js
@@ -0,0 +1,87 @@
+const Card = require('../models/cards');
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => {
+      res.status(200).send(cards);
+    })
+    .catch(() => {
+      res.status(500).send({ message: 'Произошла ошибка' });
+    });
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link, owner = req.user._id } = req.body;
+  Card.create({ name, link, owner })
+    .then((card) => {
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError' || err.name === 'ValidationError') {
+        return res
+          .status(400)
+          .send({
+            message: 'При создании карточки данные переданы некорректно',
+          });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка на сервере' });
+    });
+};
+
+module.exports.deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      if (card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      return res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res
+          .status(400)
+          .send({ message: 'Ошибка валидации. Карточка не найдена' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка на сервере' });
+    });
+};
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      if (card) {
+        return res.status(200).send(card);
+      }
+      return res.status(404).send({ message: 'Карточка не найдена' });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError' || err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Данные переданы некорректно' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка на сервере' });
+    });
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      if (card) {
+        return res.status(200).send(card);
+      }
+      return res.status(404).send({ message: 'Карточка не найдена' });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Данные переданы некорректно' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка на сервере' });
+    });
+};
